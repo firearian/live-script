2298941d16c416071d57195d1eb1a616b14a1855
Azul Alysum <Azul.Alysum@ibm.com>
2023-06-17 05:07:16 +1200

    Update App.js
    Updated App to follow the Single Responsibility Principle.
    Encapsulated handleRoomClick, toggleMenu, and logoutUser
    Created different contexts (useCurrentContext, useWebSocketContext, and useLocalStorageContext)

--- live-script/src/App.js
+++ live-script/src/App.js
@@ -1,55 +1,60 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useCurrentContext } from './Contexts/CurrentContext';
+import { useWebSocketContext } from './Contexts/WebSocketContext';
+import { useLocalStorageContext } from './Contexts/LocalStorageContext';
+import useLogout from './useLogout';
+import Routers from './routes';
+import LogoutButton from './Components/LogoutButton';
+import MenuContainer from './Components/MenuContainer';
 import './App.css';
 import './index.css';
-import { useState, useEffect, useMemo } from 'react';
-import Routers from './routes';
-import LogoutContext from './Contexts/ContextProviders';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [hasLoggedOut, setHasLoggedOut] = useState(true);
-  const closeApp = () => {
-    console.log('CloseApp renders.');
-    localStorage.removeItem('lstoken');
-    window.location.href = '../';
-    setHasLoggedOut(true);
-  };
-  console.log('App renders.');
+  const { setIsAuthenticated, currentRoom, setCurrentRoom, removeCurrentContext, selectedRoom } =
+    useCurrentContext();
+  const { resetProvider, changeRoom } = useWebSocketContext();
+  const [isOpen, setIsOpen] = useState(false);
+  const { token, removeToken, user, removeUser } = useLocalStorageContext();
 
-  const contextValue = useMemo(
-    () => ({
-      hasLoggedOut,
-      setHasLoggedOut,
-    }),
-    [hasLoggedOut, setHasLoggedOut],
-  );
+  const handleLogout = useLogout();
 
   useEffect(() => {
-    console.log('App useEffect initial call rendered.');
-    const token = localStorage.getItem('lstoken');
-    // verfity jwt token
-    if (token) {
+    if (token && currentRoom && user) {
       setIsAuthenticated(true);
-      setHasLoggedOut(false);
     }
-  }, []);
+  }, [user, currentRoom, token]);
+
+  const handleRoomClick = useCallback(
+    (newRoom) => {
+      if (newRoom !== currentRoom) {
+        setCurrentRoom(newRoom);
+        changeRoom(newRoom);
+      }
+      setIsOpen(false);
+    },
+    [currentRoom, setCurrentRoom, changeRoom, setIsOpen],
+  );
+
+  const toggleMenu = useCallback(() => {
+    setIsOpen(!isOpen);
+  }, [isOpen, setIsOpen]);
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <LogoutContext.Provider value={contextValue}>
-          <div className='editor__header'>
-            {isAuthenticated && (
-              <button
-                type='button'
-                onClick={closeApp}
-                className='group relative w-1/8 flex mr-2 justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-              >
-                Sign out
-              </button>
-            )}
-          </div>
-          <Routers isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
-        </LogoutContext.Provider>
+        {user?.documents && (
+          <MenuContainer
+            isOpen={isOpen}
+            toggleMenu={toggleMenu}
+            roomArray={user.documents}
+            selectedRooms={selectedRoom}
+            handleRoomClick={handleRoomClick}
+          />
+        )}
+        <div className='editor__header'>
+          <LogoutButton closeApp={handleLogout} />
+        </div>
+        <Routers />
       </header>
     </div>
   );
