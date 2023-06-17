3fdb56040b107f2c3b8f84f0710aedd570dd6c38
Azul Alysum <Azul.Alysum@ibm.com>
2023-06-17 22:02:25 +1200

    Update WebSocketProvider.jsx
    Context provider creation

--- live-script/src/Contexts/WebSocketProvider.jsx
+++ live-script/src/Contexts/WebSocketProvider.jsx
@@ -1,43 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext, useMemo, useContext } from 'react';
 import { HocuspocusProviderWebsocket } from '@hocuspocus/provider';
-import * as Y from 'yjs';
-
-// export const yDocInstance = new Y.Doc();
-// const WebSocketContext = createContext();
-
-// export function WebSocketProvider({ children }) {
-//   const yDoc = yDocInstance;
-//   const websocketProvider = useMemo(
-//     () =>
-//       new HocuspocusProvider({
-//         url: 'ws://0.0.0.0:3001/api/collaboration/:document',
-//         name: 'room',
-//         document: yDoc,
-//         token: localStorage.getItem('lstoken'),
-//         connect: false,
-//         onConnect() {
-//           console.log('WS connected!');
-//         },
-//         onOpen() {
-//           console.log('WS Opened!');
-//         },
-//       }),
-//     [],
-//   );
-
-//   return (
-//     <WebSocketContext.Provider value={websocketProvider}>{children}</WebSocketContext.Provider>
-//   );
-// }
-
-// export const useWebSocket = () => {
-//   const websocketProvider = useContext(WebSocketContext);
-//   if (!websocketProvider) {
-//     throw new Error('useWebSocket must be used within a WebSocketProvider');
-//   }
-//   return websocketProvider;
-// };
 
 const websocketProvider = new HocuspocusProviderWebsocket({
   url: 'ws://0.0.0.0:3001/api/collaboration/:document',
@@ -61,9 +23,6 @@ const websocketProvider = new HocuspocusProviderWebsocket({
   },
   onAuthenticationFailed() {
     console.log('WS Auth Failed');
-  },
-  onMessage(message) {
-    console.log('got a message', message);
   },
   onOutgoingMessage(message) {
     console.log('sending message: ', message);
