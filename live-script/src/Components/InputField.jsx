9b4ca823447517d5078e26090b8606cbe1d6014e
Azul Alysum <Azul.Alysum@ibm.com>
2023-06-17 22:05:37 +1200

    Create InputField.jsx

---
+++ live-script/src/Components/InputField.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+
+const InputField = function ({ id, type, value, onChange, placeholder }) {
+  return (
+    <div>
+      <label htmlFor={id}>
+        {placeholder}
+        <input
+          id={id}
+          name={id}
+          type={type}
+          autoComplete={type}
+          required
+          value={value}
+          onChange={onChange}
+          className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+          placeholder={placeholder}
+        />
+      </label>
+    </div>
+  );
+};
+InputField.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+};
+
+InputField.defaultProps = {
+  id: '',
+  type: '',
+  value: '',
+  onChange: () => {},
+  placeholder: PropTypes.string,
+};
+
+export default InputField;
