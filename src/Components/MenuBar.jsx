f9db826c34f8d9b620c625b8677026c247ee00c5
Azul Alysum <Azul.Alysum@ibm.com>
2023-06-29 04:34:51 +1200

    Fix menuitems and clean files
    Remove unnecessary files
    Fix prop passing for menuitem

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -1,4 +1,4 @@
-import { Editor, isActive } from '@tiptap/react';
+import { Editor } from '@tiptap/react';
 import PropTypes from 'prop-types';
 import './MenuBar.css';
 
@@ -27,12 +27,6 @@ const MenuBar = function ({ editor }) {
       isActive: () => editor.isActive('strike'),
     },
     {
-      icon: 'code-view',
-      title: 'Code',
-      action: () => editor.chain().focus().toggleCode().run(),
-      isActive: () => editor.isActive('code'),
-    },
-    {
       icon: 'mark-pen-line',
       title: 'Highlight',
       action: () => editor.chain().focus().toggleHighlight().run(),
@@ -40,48 +34,6 @@ const MenuBar = function ({ editor }) {
     },
     {
       type: 'divider',
-    },
-    {
-      icon: 'h-1',
-      title: 'Heading 1',
-      action: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
-      isActive: () => editor.isActive('heading', { level: 1 }),
-    },
-    {
-      icon: 'h-2',
-      title: 'Heading 2',
-      action: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
-      isActive: () => editor.isActive('heading', { level: 2 }),
-    },
-    {
-      icon: 'paragraph',
-      title: 'Paragraph',
-      action: () => editor.chain().focus().setParagraph().run(),
-      isActive: () => editor.isActive('paragraph'),
-    },
-    {
-      icon: 'list-unordered',
-      title: 'Bullet List',
-      action: () => editor.chain().focus().toggleBulletList().run(),
-      isActive: () => editor.isActive('bulletList'),
-    },
-    {
-      icon: 'list-ordered',
-      title: 'Ordered List',
-      action: () => editor.chain().focus().toggleOrderedList().run(),
-      isActive: () => editor.isActive('orderedList'),
-    },
-    {
-      icon: 'list-check-2',
-      title: 'Task List',
-      action: () => editor.chain().focus().toggleTaskList().run(),
-      isActive: () => editor.isActive('taskList'),
-    },
-    {
-      icon: 'code-box-line',
-      title: 'Code Block',
-      action: () => editor.chain().focus().toggleCodeBlock().run(),
-      isActive: () => editor.isActive('codeBlock'),
     },
     {
       type: 'divider',
@@ -98,17 +50,9 @@ const MenuBar = function ({ editor }) {
       action: () => editor.chain().focus().setHorizontalRule().run(),
     },
     {
-      type: 'divider',
-    },
-    {
       icon: 'text-wrap',
       title: 'Hard Break',
       action: () => editor.chain().focus().setHardBreak().run(),
-    },
-    {
-      icon: 'format-clear',
-      title: 'Clear Format',
-      action: () => editor.chain().focus().clearNodes().unsetAllMarks().run(),
     },
     {
       type: 'divider',
@@ -128,19 +72,14 @@ const MenuBar = function ({ editor }) {
   return (
     <div className='editor__footer'>
       {items.map((item, index) => {
-        const { icon, title, action, isActiveFunction } = item;
+        const { icon, title, action, isActive } = item;
         const key = item.icon ? item.icon : item.type + index;
         return (
           <Fragment key={key}>
             {item.type === 'divider' ? (
               <div className='divider' />
             ) : (
-              <MenuItem
-                icon={icon}
-                title={title.toString()}
-                onClick={action}
-                isActive={isActiveFunction}
-              />
+              <MenuItem icon={icon} title={title.toString()} action={action} isActive={isActive} />
             )}
           </Fragment>
         );
